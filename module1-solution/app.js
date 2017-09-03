0a7c3d5abca6d663f11520fab2522112ab7a8e97
Alex Hepting <alex.hepting@gmail.com>
2017-09-03 03:12:10 +0200

    initial commit

---
+++ module1-solution/app.js
@@ -0,0 +1,42 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope){
+  $scope.text = "";
+  $scope.message = "";
+  
+  var comma = ',';
+  
+  function splitString(stringToSplit, separator)
+  {
+   var arrayOfStrings = stringToSplit.split(separator);
+   
+   return arrayOfStrings.length
+  };
+  
+  $scope.check = function () {
+   
+   if ($scope.text == "")
+   {
+    $scope.message = "Please enter data first";
+   }   
+   
+   if (splitString($scope.text, comma)<=3) 
+   {
+    $scope.message = "Enjoy!";
+   }
+   else
+   {
+    $scope.message = "Too much!";
+   }
+  
+  };
+
+};
+
+})();
